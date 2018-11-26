6a6cf9179e22968b24698c844fdbaaac507ffdf7
fringedroid <gaurav901110@gmail.com>
2018-11-26 03:10:42 -0500

    add setup cluster and namenode to datanode communication

---
+++ src/Config.py
@@ -0,0 +1,11 @@
+
+DATABASE_NAME = 'hdfs'
+
+PROJECT_DATA_PATH = '/home/droid/project_data/datanodes/'
+
+NAMENODE_LOCATION = 'nnode'
+
+DATANODE_LOCATION = 'dnode'
+DATANODE_ID_FILE = 'datanodeid'
+
+DATANODE_HEARTBEAT_INTERVAL = 5
